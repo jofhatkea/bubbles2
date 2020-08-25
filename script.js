cc6999f012fe41dca55b3d97eda57184a8585372
Jonas Holbech <jh@jonasholbech.dk>
2020-08-25 11:54:27 +0200

    move bubs

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function setupCircles() {
   const cs = document.querySelectorAll("div");
   cs.forEach(el => {
     el.style.transform = `translate(${Math.random()*100}vw,${Math.random()*100}vh)`;
+    el.addEventListener("mouseover", c => {
+      el.style.background = `hsl(${Math.random()*360},70%,70%)`;
+      el.style.transform = `translate(${Math.random()*100}vw,${Math.random()*100}vh)`;
+    })
   })
 
 }
